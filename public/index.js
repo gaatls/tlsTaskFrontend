488386caef465000f69713f5afbd3b88c336810e
Hayden Wagner (Student Employee) <tlshbw@ad.rit.edu>
2016-09-19 14:06:34 -0400

    Form format changes, added basic validation plugin

--- public/index.js
+++ public/index.js
@@ -8,6 +8,14 @@ socket.on('connected', function (data) {
     socket.emit('send back', "alright");
 });
 
+
+
+/**
+ * Shows field when radio button is toggled...may be better/more efficient way to
+ * do with bootstrap but I couldn't find it so I'm moving on.
+ * 
+ * @param {string} val String value stored on the toggled radio button
+ */
 function handleRadioRequestMadeByProf(val){
     var el;
     
@@ -15,14 +23,12 @@ function handleRadioRequestMadeByProf(val){
        el = $('#professor-detail-input_not-hidden')
        
        el.attr('id','professor-detail-input_hidden');
-       //el.attr("required", false);
        el.find('input').attr("required", false);
     }
     else{
        el = $('#professor-detail-input_hidden');
 
        el.attr('id','professor-detail-input_not-hidden');
-       //el.attr("required", true);
        el.find('input').attr("required", true);
     }
 }
