4168db7242d236adf24beb616a30b62e34ebb240
haydenwagner <haydenblainewagner@gmail.com>
2016-09-20 13:47:29 -0400

    Updates tlsAsna dependenc-attempt at task creation --broken but I have to go to class

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = require('express')();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let tlsAsana = require('tls-asana');
-let asanaConnection = null;
+let tlsAsanaPromise = null;
 
 server.listen(8001);
 
@@ -14,25 +14,34 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
 io.on('connection', function (socket) {
-  console.log("Successfully connected");
-  socket.emit('connected', "Successfully connected");
+    console.log("Successfully connected");
   
-  socket.on('send back', function (data) {
-      console.log(data);
-  });
+    if(tlsAsana){
+        tlsAsanaPromise = tlsAsana.connect(166216691534199).then(client => {
+            console.log('Asana Connected and setup');
+            //   tlsAsana.checkTagCache().then(res=>{
+            //     console.log(res);
+            //   })
+        });
+    }
+  
+  
+    socket.emit('connected', "Successfully connected");
+    
+    socket.on('send back', function (data) {
+        console.log(data);
+    });
 
-  socket.on('form-submission', function (data) {
-      console.log(data);
-  });
+    socket.on('form-submission', function(data) {
+        console.log(data);
+        
+        //tlsAsanaPromise.then(function(){
+            tlsAsana.createTask('tester9', data).then(response => {
+                console.log(res);
+                console.log('task inputted Successfully');
+            });
+        //})
+    });
 
   //------------
-  if(tlsAsana){
-      tlsAsana.connect(166216691534199).then(client => {
-          //asanaConnection = client;
-          console.log('Asana Connected and setup');
-        //   tlsAsana.checkTagCache().then(res=>{
-        //     console.log(res);
-        //   })
-      });
-  }
 });
