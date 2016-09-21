47198904cdef6ac59f3104d0eb30da67796bb638
haydenwagner <haydenblainewagner@gmail.com>
2016-09-21 15:25:32 -0400

    Cleans up socket events, moves form submission to its own function instead of anonomous socket func

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let express = require('express');
 let app = require('express')();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
+let _ = require('lodash');
 let tlsAsana = require('tls-asana');
 let tlsAsanaPromise = null;
 
@@ -14,37 +15,67 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
 io.on('connection', function (socket) {
-    console.log("Successfully connected");
+    console.log("Successful socket connection");
   
     if(tlsAsana){
         tlsAsanaPromise = tlsAsana.connect('166216691534199').then(client => {
-            console.log('Asana Connected and setup');
-            //   tlsAsana.checkTagCache().then(res=>{
-            //     console.log(res);
-            //   })
+            console.log('Asana connected and setup');
         });
     }
   
-  
     socket.emit('connected', "Successfully connected");
-    
-    socket.on('send back', function (data) {
-        console.log(data);
+
+    socket.on('form-submission', data => {
+        socketFormSubmission(data);  
     });
-
-    socket.on('form-submission', function(data) {
-        console.log(data);
-
-        tlsAsanaPromise.then(function(){
-            tlsAsana.createTask('testtask10', data).then(response => {
-                console.log(response);
-                console.log('task inputted Successfully');
-
-                socket.emit('task-input-success', response);
-            });
-        });
-
-    });
-
-  //------------
 });
+
+
+
+/**
+ * Handles a submission to create a new task from the tls task input frontend form
+ * 
+ * param {object} data - Task data from the submitted form
+ */
+function socketFormSubmission(data){
+    let taskName = generateTaskName(data);
+
+    tlsAsanaPromise.then(function(){
+        tlsAsana.createTask(taskName, data).then(response => {
+            console.log(response);
+            console.log('task inputted Successfully');
+
+            socket.emit('task-input-success', response);
+        });
+    });
+}
+
+
+
+/**
+ * Creates a task name when a task is to be created; title is made up of
+ * various pieces of data from the use form submission, that can be editted by
+ * changing the 'titleFields' var
+ * 
+ * param {Object} data - Task data from the submitted form
+ */
+function generateTaskName(data){
+    let taskNameStr = "";
+
+    //edit if we want to change the structure of the titles in asana
+    let titleFields = ['type', 'date', 'courseID', 'nameRequester'];
+
+    _.forEach(titleFields, function(x){
+        if( data[x] ){
+            //if else just adds space between fields in name after the first one
+            if(taskNameStr === ""){
+                taskNameStr = taskNameStr + data[x];
+            }
+            else{
+                taskNameStr = taskNameStr + " - " + data[x];
+            }
+        }
+    })
+
+    return taskNameStr;
+}
