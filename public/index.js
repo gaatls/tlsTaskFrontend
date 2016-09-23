c046061ea554dee86215a27d6a33ef098481af4d
Hayden Wagner (Student Employee) <tlshbw@ad.rit.edu>
2016-09-23 19:12:09 -0400

    Adds response modal to frontend

--- public/index.js
+++ public/index.js
@@ -13,6 +13,7 @@ socket.on('connected', function (data) {
 
 socket.on('task-input-success', function(data){
     console.log(data);
+    taskInputSuccess(data);
     //do anything we need to do after a successful task input (notify user, give them next steps)
 });
 
@@ -25,15 +26,6 @@ socket.on('task-input-failure', function(data){
 
 
 $( document ).ready(function docReady(){
-
-    //loading screen test
-
-    waitingDialog.show('Test task input loading screen');
-    setTimeout(function () {
-        waitingDialog.hide();
-    }, 2500);
-
-    // end loading screen test
 
     $('#tlsTaskForm').validator().on('submit', function (e) {
         if (e.isDefaultPrevented()) {
@@ -59,14 +51,15 @@ function handFormSubmission(fields){
     data = filterFields(fields, data);
     data = filterCheckboxes(data);
 
-    console.log(data);
-
     //send our form data to the server
     socket.emit('form-submission', data);
+    taskInputPending();
 }
 
 
-
+/**
+ * 
+ */
 function filterFields(fields, data){
     var filteredFormFields = _.filter(fields, function(x){
        return _.includes(addFieldsToData, x.id);
@@ -82,7 +75,9 @@ function filterFields(fields, data){
 }
 
 
-
+/**
+ * 
+ */
 function filterCheckboxes(data){
     _.forEach(addCheckBoxToData, function(x){
         var el = $( 'input[name="type"]:checked' );
@@ -155,4 +150,87 @@ function restoreDeletedValues(fields, values){
             el[0].value = values[x];
         }
     })
+}
+
+
+/**
+ * 
+ */
+function taskInputPending(){
+    $('.task-input-modal, .task-input-modal-backdrop').css('display','block');
+}
+
+
+/**
+ * 
+ */
+function taskInputSuccess(taskData){
+    var taskData = processAsanaData(taskData);
+    var modalContainer = $('.task-input-modal');
+    var taskInfoEl = modalContainer.find('#tim-task-info');
+
+    modalContainer.find('#tim-progress').css('display','none');
+    modalContainer.find('#tim-header').html('Task Successfully Created');//.css('color','#449d44');
+    $(taskInfoEl).css('display', 'block');
+    modalContainer.find('button').each(function(i){ $(this).css('display','block'); });
+
+    _.forEach(taskData, function(val,key){
+        $(taskInfoEl).append(key + ': ' + val + '<br>');
+    })
+}
+
+/**
+ * 
+ */
+function taskInputSuccessRevert(){
+    var modalContainer = $('.task-input-modal');
+
+    modalContainer.find('#tim-progress').css('display','block');
+    modalContainer.find('#tim-header').html('Task Input Pending');//.css('color','#449d44');
+    modalContainer.find('#tim-task-info').html("").css('display','none');
+    modalContainer.find('button').each(function(i){ $(this).css('display','none'); });
+}
+
+/**
+ * 
+ */
+function processAsanaData(taskData){
+    var parsedExternalData = JSON.parse(taskData.external.data);
+    
+    var returnData = {
+        'Task Name         ': taskData.name,
+        'Requested by      ': parsedExternalData.nameRequester,
+        'RIT Email         ': parsedExternalData.emailRequester,
+        'Course ID         ': parsedExternalData.courseID,
+        'Request Type      ': parsedExternalData.type,
+        'Date/Time Created ': parsedExternalData.titleDate,
+    }
+
+    //check for, and add, optional return data
+    if(parsedExternalData.nameProfessor){
+        returnData["Professor Name    "] = parsedExternalData.nameProfessor;
+    }
+    if(parsedExternalData.emailProfessor){
+        returnData["Professor Email   "] = parsedExternalData.emailProfessor;
+    }
+
+    return returnData;
+}
+
+/**
+ * 
+ */
+function taskInputComplete(clear){
+    $('.task-input-modal, .task-input-modal-backdrop').css('display','none');
+    taskInputSuccessRevert();
+
+    if(clear){
+        $('#tlsTaskForm').find("input[type=text], input[type=email]").each(function(){
+            $(this).val("").css('background-color', '#fff !important');
+        });
+
+        $('#requestMadeByProf-1').prop('checked',false).button('refresh');
+        $('#requestMadeByProf-0').prop('checked',true).button('refresh');
+        handleRadioRequestMadeByProf("true");
+    }
 }
